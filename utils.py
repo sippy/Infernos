7a6148d6a61b23ff94b8aab2d636df61c83b7444
Maksym Sobolyev <sobomax@sippysoft.com>
2023-09-22 18:50:30 +0000

    Add utils.

---
+++ utils.py
@@ -0,0 +1,17 @@
+import os, glob
+import torch
+
+def load_checkpoint(filepath, device):
+    assert os.path.isfile(filepath)
+    print("Loading '{}'".format(filepath))
+    checkpoint_dict = torch.load(filepath, map_location=device)
+    print("Complete.")
+    return checkpoint_dict
+
+def scan_checkpoint(cp_dir, prefix):
+    pattern = os.path.join(cp_dir, prefix + '????????')
+    cp_list = glob.glob(pattern)
+    if len(cp_list) == 0:
+        return None
+    return sorted(cp_list)[-1]
+
