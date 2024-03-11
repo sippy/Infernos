bb7ecbd318f5c6c586e655add9aba9d4d7ddb4b1
Maksym Sobolyev <sobomax@sippysoft.com>
2024-03-11 06:13:06 -0700

    Pass chunks into infer function.

--- Cluster/STTSession.py
+++ Cluster/STTSession.py
@@ -7,9 +7,12 @@ class STTSession():
     def __init__(self, stt):
         super().__init__()
         self.id = uuid4()
+        self.stt = stt
 
     def stop(self):
         if self.debug: print('STTSession.stop')
+        del self.stt
 
     def soundin(self, chunk):
         if self.debug: print(f'STTSession.soundin({len(chunk)=})')
+        self.stt.infer(self, chunk, None)
