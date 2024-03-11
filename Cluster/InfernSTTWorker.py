499751b759f246d80c470c9c0df681e2f3492a9f
Maksym Sobolyev <sobomax@sippysoft.com>
2024-03-11 06:36:26 -0700

    Make and process batches.

--- Cluster/InfernSTTWorker.py
+++ Cluster/InfernSTTWorker.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from queue import Queue
+from queue import Queue, Empty as QueueEmpty
 
 import torch
 import ctranslate2
@@ -15,6 +15,7 @@ class STTWI():
     res_queue: Queue
 
 class InfernSTTWorker(InfernWrkThread):
+    max_batch_size: int = 4
     model: ctranslate2.models.Whisper
     processor: transformers.WhisperProcessor
     device: str
@@ -31,13 +32,39 @@ class InfernSTTWorker(InfernWrkThread):
         wi.stt_sess, wi.audio, wi.res_queue = stt_sess, audio, res_queue
         self.inf_queue.put(wi)
 
+    def next_batch(self):
+        wis = []
+        while len(wis) < self.max_batch_size:
+            if len(wis) == 0:
+                wi = self.inf_queue.get()
+            else:
+                try: wi = self.inf_queue.get_nowait()
+                except QueueEmpty: break
+            if wi is None:
+                return None
+            wis.append(wi)
+        return wis
+
     def run(self):
         super().thread_started()
         while self.get_state() == RTPWrkTRun:
-            wi = self.inf_queue.get()
-            if wi is None:
+            wis = self.next_batch()
+            if wis is None:
                 break
-            print(f'InfernSTTWorker.run: got {wi} from inf_queue')
+            print(f'InfernSTTWorker.run: got {wis} from inf_queue')
+            audios = [wi.audio for wi in wis]
+            inputs = self.processor(audios, return_tensors="np", sampling_rate=16000)
+            features = ctranslate2.StorageView.from_array(inputs.input_features)
+            results = self.model.detect_language(features)
+            prompt = [self.processor.tokenizer.convert_tokens_to_ids(
+                [
+                    "<|startoftranscript|>",
+                    language,
+                    "<|transcribe|>",
+                    "<|notimestamps|>",  # Remove this token to generate timestamps.
+                ]) for language in (r[0][0] for r in results)]
+            results = self.model.generate(features, prompt)
+            for r in results: print(self.processor.decode(r.sequences_ids[0]))
             #with torch.no_grad():
             #    audio = wi.audio.to(self.device)
             #    res = wi.stt_sess.model(audio)
