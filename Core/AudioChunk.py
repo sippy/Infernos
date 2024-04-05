cacfc47e693f958aa6fa6c69b4299463408a77b7
Maksym Sobolyev <sobomax@sippysoft.com>
2024-04-04 23:39:59 -0700

    Add AudioChunkFromURL.

--- Core/AudioChunk.py
+++ Core/AudioChunk.py
@@ -1,4 +1,9 @@
 import torch
+import requests
+import soundfile as sf
+from io import BytesIO
+
+from config.InfernGlobals import InfernGlobals as IG
 
 class AudioChunk():
     samplerate: int
@@ -8,3 +13,13 @@ class AudioChunk():
         assert isinstance(audio, torch.Tensor)
         self.audio = audio
         self.samplerate = samplerate
+
+class AudioChunkFromURL(AudioChunk):
+    def __init__(self, url:str, samplerate=8000, dtype=torch.float16, **kwargs):
+        response = requests.get(url)
+        sound_bytes = BytesIO(response.content)
+        audio, samplerate_in = sf.read(sound_bytes)
+        audio = torch.from_numpy(audio)
+        if samplerate_in != samplerate:
+            audio = IG.get_resampler(samplerate_in, samplerate)(audio.to(torch.float))
+        super().__init__(audio.to(dtype), samplerate, **kwargs)
