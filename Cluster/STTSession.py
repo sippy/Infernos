6bfd33839619fc9bd41aa629b9c93a4bcbdb87e3
Maksym Sobolyev <sobomax@sippysoft.com>
2024-04-12 00:50:56 -0700

    Serialize subsequent requests from the same STT session.

--- Cluster/STTSession.py
+++ Cluster/STTSession.py
@@ -1,6 +1,8 @@
 from typing import List, Optional
 from uuid import uuid4, UUID
 from fractions import Fraction
+from functools import partial
+from threading import Lock
 
 import torch
 
@@ -25,17 +27,43 @@ class STTSession():
     id: UUID
     lang: str = 'en'
     context: List[int]
+    state_lock: Lock
+    busy: bool = False
+    pending: List[STTRequest]
 
     def __init__(self, stt, keep_context:bool):
         super().__init__()
         self.id = uuid4()
         self.stt = stt
+        self.state_lock = Lock()
         self.context = [] if keep_context else None
+        self.pending = []
 
     def stop(self):
         if self.debug: print('STTSession.stop')
-        del self.stt
+        with self.state_lock:
+            del self.stt, self.pending
 
     def soundin(self, req:STTRequest):
         if self.debug: print(f'STTSession.soundin({len(req.audio)=})')
+        with self.state_lock:
+            if self.busy:
+                self.pending.append(req)
+                return
+            self.busy = True
+        req.text_cb = partial(self.tts_out, req.text_cb)
         self.stt.infer((req, self.context))
+
+    def tts_out(self, text_cb, result:STTResult):
+        with self.state_lock:
+            if not hasattr(self, 'stt'):
+                return
+            if self.debug: print(f'STTSession.tts_out({result.text=})')
+            assert self.busy
+            if self.pending:
+                req = self.pending.pop(0)
+                req.text_cb = partial(self.tts_out, req.text_cb)
+                self.stt.infer((req, self.context))
+            else:
+                self.busy = False
+        text_cb(result=result)
