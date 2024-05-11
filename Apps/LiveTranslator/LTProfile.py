48840e2df4e503358febff72fff8d1d12c374439
Maksym Sobolyev <sobomax@sippysoft.com>
2024-05-11 03:27:03 -0700

    Add a first take on the configuration file parser to replace command line.

---
+++ Apps/LiveTranslator/LTProfile.py
@@ -0,0 +1,33 @@
+from typing import Tuple, Optional
+
+import ray
+
+from Cluster.InfernSIPActor import InfernSIPActor
+
+from .LTActor import LTActor
+
+class LTProfile():
+    name: str
+    tts_langs: Tuple[str]
+    stt_langs: Tuple[str]
+    _outbound_spec: str
+    outbound_conn: 'InfernSIPProfile'
+    outbount_params: str
+    actor: Optional[LTActor] = None
+
+    def __init__(self, name, conf):
+        self.name = name
+        self.tts_langs = tuple(conf['tts_langs'])
+        self.stt_langs = tuple(conf['stt_langs'])
+        self._outbound = conf['outbound']
+
+    def finalize(self, iconf:'InfernConfig'):
+        sip_cname, params = self._outbound.split(';', 1)
+        self.outbound_conn = iconf.connectors[sip_cname]
+        self.outbount_params = params
+
+    def getActor(self, iconf:'InfernConfig', sip_act:InfernSIPActor):
+        if self.actor is None:
+            self.actor = LTActor.remote()
+            ray.get(self.actor.start.remote(sip_act))
+        return self.actor
---
+++ Core/InfernConfig.py
@@ -0,0 +1,114 @@
+from typing import Dict
+
+import yaml
+from cerberus import Validator
+
+def validate_port_range(field, value, error):
+    if ':' in value:
+        _, port = value.split(':', 1)
+        if not (1 <= int(port) <= 65535):
+            error(field, 'Port number must be in the range 1-65535')
+
+# Define the schema
+schema = {
+    'sip': {
+        'type': 'dict',
+        'schema': {
+            'settings': {
+                'type': 'dict',
+                'schema': {
+                    'bind': {
+                        'type': 'string',
+                        'regex': r'^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}(:[1-9][0-9]{0,4}|:0)?$',
+                        'check_with': validate_port_range
+                    },
+                }
+            },
+            'profiles': {
+                'type': 'dict',
+                'keysrules': {'type': 'string'},
+                'valuesrules': {
+                    'type': 'dict',
+                    'schema': {
+                        'sip_server': {
+                            'type': 'string',
+                            'regex': r'^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}(:[1-9][0-9]{0,4}|:0)?$',
+                            'check_with': validate_port_range
+                        },
+                        'username': {'type': 'string'},
+                        'password': {'type': 'string'},
+                        'register': {'type': 'boolean'},
+                        'sink': {'type': 'string'},
+                    }
+                }
+            }
+        }
+    },
+    'apps': {
+        'type': 'dict',
+        'schema': {
+            'live_translator': {
+                'type': 'dict',
+                'schema': {
+                    'profiles': {
+                        'type': 'dict',
+                        'keysrules': {'type': 'string'},
+                        'valuesrules': {
+                            'type': 'dict',
+                            'schema': {
+                                'tts_langs': {'type': 'list', 'schema': {'type': 'string'}},
+                                'stt_langs': {'type': 'list', 'schema': {'type': 'string'}},
+                                'outbound': {'type': 'string'}
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+class InfernConfigParseErr(Exception): pass
+
+def validate_yaml(filename):
+    try:
+        with open(filename, 'r') as file:
+            data = yaml.safe_load(file)
+
+        v = Validator(schema)
+        if not v.validate(data):
+            raise InfernConfigParseErr(f"Validation errors in {filename}: {v.errors}")
+
+    except yaml.YAMLError as exc:
+        raise InfernConfigParseErr(f"Error parsing YAML file {filename}: {exc}") from exc
+    return data
+
+from Cluster.InfernSIPActor import InfernSIPActor
+from Apps.LiveTranslator.LTProfile import LTProfile
+from SIP.InfernUA import InfernSIPConf
+from SIP.InfernSIPProfile import InfernSIPProfile
+
+class InfernConfig():
+    sip_actr: InfernSIPActor
+    sip_conf: InfernSIPConf
+    connectors: Dict[str, InfernSIPProfile]
+    apps: Dict[str, LTProfile]
+    def __init__(self, filename: str):
+        d = validate_yaml(filename)
+        self.sip_conf = InfernSIPConf()
+        try:
+            bind = d['sip']['settings']['bind'].split(':', 1)
+        except KeyError: pass
+        else:
+            port = int(bind[1]) if len(bind) == 2 else self.sip_conf.lport
+            self.sip_conf.laddr = bind[0]
+            self.sip_conf.lport = port
+        self.connectors = dict((f'sip/{name}', InfernSIPProfile(name, conf))
+                               for name, conf in d['sip']['profiles'].items())
+        self.apps = dict((f'apps/live_translator/{name}', LTProfile(name, conf))
+                         for name, conf in d['apps']['live_translator']['profiles'].items())
+        for app in self.apps.values():
+            app.finalize(self)
+        self.sip_actr = InfernSIPActor.options(max_concurrency=2).remote()
+        for conn in self.connectors.values():
+            conn.finalize(self.sip_actr, self)
